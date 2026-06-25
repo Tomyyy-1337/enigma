3eaa796da6f49b5380c39eb2d86ba0cf3f6207ef
tom <tom.passberg@hotmail.com>
2026-06-25 23:06:55 +0200

    crack enigma success

--- src/enigma.rs
+++ src/enigma.rs
@@ -1,6 +1,9 @@
+use std::fmt::Debug;
+
 use crate::{Eintrittswalze, Umkehrwalze, Walze};
 
 /// A struct representing an Enigma machine with a configurable number of rotors (N_WALZEN).
+#[derive(Clone)]
 pub struct Enigma<const N_WALZEN: usize> {
     eintrittswalze: &'static Eintrittswalze,
     walzen: [&'static Walze; N_WALZEN],
@@ -10,12 +13,25 @@ pub struct Enigma<const N_WALZEN: usize> {
     steckbrett: [u8; 26],
 } 
 
-#[derive(Debug)]
+
 pub enum EnigmaError {
     InvalidPlugboardConnection(String),
     OverlappingPlugboardConnection(String),
     InvalidWalzenStellung(u8),
     InvalidRingstellung(u8),
+    InvalidCharactersInInput(char),
+}
+
+impl Debug for EnigmaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EnigmaError::InvalidPlugboardConnection(pair) => write!(f, "Invalid plugboard connection: {}. Each connection must be a pair of uppercase letters (A-Z) and cannot contain duplicates.", pair),
+            EnigmaError::OverlappingPlugboardConnection(pair) => write!(f, "Overlapping plugboard connection: {}. Each letter can only be connected to one other letter.", pair),
+            EnigmaError::InvalidWalzenStellung(stellung) => write!(f, "Invalid Walzenstellung: {}. Each rotor position must be between 1 and 26.", stellung),
+            EnigmaError::InvalidRingstellung(stellung) => write!(f, "Invalid Ringstellung: {}. Each ring setting must be between 1 and 26.", stellung),
+            EnigmaError::InvalidCharactersInInput(c) => write!(f, "Invalid character in input: {}. Only uppercase letters (A-Z) are allowed.", c),
+        }
+    }
 }
 
 impl<const N_WALZEN: usize> Enigma<N_WALZEN> {
@@ -49,9 +65,7 @@ impl<const N_WALZEN: usize> Enigma<N_WALZEN> {
 
     /// Unplugs all plugboard connections, resetting the plugboard to its default state where each letter maps to itself.
     pub fn reset_plugboard(&mut self) {
-        for i in 0..26 {
-            self.steckbrett[i] = i as u8;
-        }
+        self.steckbrett = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];    
     }
 
     /// Sets the plugboard connections based on a string of space-separated letter pairs.
@@ -75,14 +89,47 @@ impl<const N_WALZEN: usize> Enigma<N_WALZEN> {
         Ok(())
     } 
 
+    pub fn set_plugboard_unchecked(&mut self, connections: &[[char; 2]]) {
+        self.reset_plugboard();
+        for &[a, b] in connections {
+            self.set_plug_unchecked(a, b);
+        }
+    }
+
+    pub fn set_plug_unchecked(&mut self, a: char, b: char) {
+        let a = (a as u8 - b'A') as usize;
+        let b = (b as u8 - b'A') as usize;
+        self.steckbrett[a] = b as u8;
+        self.steckbrett[b] = a as u8;
+    }
+
+    pub fn reset_plug_unchecked(&mut self, a: char, b: char) {
+        let a = (a as u8 - b'A') as usize;
+        let b = (b as u8 - b'A') as usize;
+        self.steckbrett[a] = a as u8;
+        self.steckbrett[b] = b as u8;
+    }
+
     /// Encodes a string of characters using the Enigma machine's current configuration.
     /// The rotors will advance with each character encoded, simulating the behavior of the original Enigma machine.
     /// 
     /// The encode function changes the rotor positions, so the same input will yield different outputs if encoded multiple times without resetting the rotor positions.
-    pub fn encode(&mut self, input: &str) -> String {
-        input.chars()
+    pub fn encode(&mut self, input: &str) -> Result<String, EnigmaError> {
+        if let Some(bad_char) = input.chars().find(|&c| !c.is_ascii_uppercase()) {
+            return Err(EnigmaError::InvalidCharactersInInput(bad_char));
+        }
+        let result = input.chars()
             .map(|c| self.encode_char(c))
-            .collect()
+            .collect();
+        Ok(result)
+    }   
+
+    pub fn encode_and_reset(&mut self, input: &str) -> Result<String, EnigmaError> {
+        let enigma_before = self.clone();
+        let walzen_stellung_before = self.walzen_stellung;
+        let result = self.encode(input);
+        self.walzen_stellung = walzen_stellung_before;
+        result
     }
 
     /// Sets the rotor positions (Walzenstellung) for each rotor.
@@ -140,11 +187,34 @@ impl<const N_WALZEN: usize> Enigma<N_WALZEN> {
 
     fn increment_walzen_stellung(&mut self) {
         for i in (0..self.walzen_stellung.len()).rev() {
-            let tmp = (self.walzen_stellung[i]) % 26;
+            let tmp = self.walzen_stellung[i];
             self.walzen_stellung[i] = (self.walzen_stellung[i] + 1) % 26;
             if !self.walzen[i].is_übertragungskerbe((tmp) as usize) {
                 break;
             }
         }
+    }
+
+    pub fn get_plugboard_mapping(&self) -> Vec<[char; 2]> {
+        let mut mapping = Vec::new();
+        for i in 0..26 {
+            let mapped_char = self.steckbrett[i];
+            if mapped_char != i as u8 && i < mapped_char as usize {
+                mapping.push([
+                    (i as u8 + b'A') as char,
+                    (mapped_char + b'A') as char,
+                ]);
+            }
+        }
+        mapping
+    }
+
+    pub fn is_plug_set(&self, c: char) -> bool {
+        let index = (c as u8 - b'A') as usize;
+        self.steckbrett[index] != index as u8
+    }
+
+    pub fn number_of_plugs_set(&self) -> usize {
+        self.steckbrett.iter().enumerate().filter(|(i, mapped)| *i != (**mapped) as usize).count() / 2
     }
 }
--- src/walze.rs
+++ src/walze.rs
@@ -1,13 +1,37 @@
+use std::ops::Deref;
+
 /// Represents the reflector (Umkehrwalze) of the Enigma machine
 pub struct Umkehrwalze {
-    mapping: [u8; 26]
+    mapping: [u8; 26],
+    name: &'static str,
+}
+
+/// Represents the entry wheel (Eintrittswalze) of the Enigma machine
+pub struct Eintrittswalze {
+    inner_walze: Umkehrwalze,
+    inverse_mapping: [u8; 26],
+}
+
+/// Represents a rotor (Walze) of the Enigma machine and its associated turnover notches (Übertragungskerben)
+pub struct Walze {
+    inner_walze: Eintrittswalze,
+    übertragungskerben: u32
 }
 
 impl Umkehrwalze {
-    pub const UKW_A: Umkehrwalze = Umkehrwalze::new("EJMZALYXVBWFCRQUONTSPIKHGD");
-    pub const UKW_B: Umkehrwalze = Umkehrwalze::new("YRUHQSLDPXNGOKMIEBFZCWVJAT");
-    pub const UKW_C: Umkehrwalze = Umkehrwalze::new("FVPJIAOYEDRZXWGCTKUQSBNMHL");
+    pub const UKW_A: Umkehrwalze = Umkehrwalze::new("EJMZALYXVBWFCRQUONTSPIKHGD")
+        .with_name("UKW A");
+    pub const UKW_B: Umkehrwalze = Umkehrwalze::new("YRUHQSLDPXNGOKMIEBFZCWVJAT")
+        .with_name("UKW B");
+    pub const UKW_C: Umkehrwalze = Umkehrwalze::new("FVPJIAOYEDRZXWGCTKUQSBNMHL")
+        .with_name("UKW C");
 
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+
+    /// Creates a new reflector (Umkehrwalze) with the specified mapping.
+    /// The mapping should be a string of 26 uppercase letters, where each letter represents the output for the corresponding input letter (A-Z). The mapping must be a valid reflector mapping, meaning that each letter must map to a different letter and the mapping must be symmetric (if A maps to B, then B must map to A).
     pub const fn new(mapping: &str) -> Self {
         let mut arr = [0; 26];
         let bytes = mapping.as_bytes();
@@ -17,8 +41,14 @@ impl Umkehrwalze {
             i += 1;
         }
         Umkehrwalze {
-            mapping: arr
+            mapping: arr,
+            name: ""
         }
+    }
+
+    pub const fn with_name(mut self, name: &'static str) -> Self {
+        self.name = name;
+        self
     }
 
     pub(crate) fn map_char(&self, c: u8) -> u8 {
@@ -26,15 +56,12 @@ impl Umkehrwalze {
     }
 }
 
-/// Represents the entry wheel (Eintrittswalze) of the Enigma machine
-pub struct Eintrittswalze {
-    inner_walze: Umkehrwalze,
-    inverse_mapping: [u8; 26],
-}
-
 impl Eintrittswalze {
-    pub const ETW: Eintrittswalze = Eintrittswalze::new("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    pub const ETW: Eintrittswalze = Eintrittswalze::new("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        .with_name("ETW");
 
+    /// Creates a new entry wheel (Eintrittswalze) with the specified mapping.
+    /// The mapping should be a string of 26 uppercase letters, where each letter represents the output for the corresponding input letter (A-Z).
     pub const fn new(mapping: &str) -> Self {
         let inner_walze = Umkehrwalze::new(mapping);
         let mut inverse_arr = [0; 26];
@@ -50,6 +77,11 @@ impl Eintrittswalze {
         }
     }
 
+    pub const fn with_name(mut self, name: &'static str) -> Self {
+        self.inner_walze = self.inner_walze.with_name(name);
+        self
+    }
+
     pub(crate) fn map_char(&self, c: u8) -> u8 {
         self.inner_walze.map_char(c)
     }
@@ -59,33 +91,56 @@ impl Eintrittswalze {
     }
 }
 
-/// Represents a rotor (Walze) of the Enigma machine and its associated turnover notches (Übertragungskerben)
-pub struct Walze{
-    inner_walze: Eintrittswalze,
-    übertragungskerben: u32
-}
-
 impl Walze {
+    pub const SAMMLUNG_I_V: [Walze; 5] = [
+        Walze::I,
+        Walze::II,
+        Walze::III,
+        Walze::IV,
+        Walze::V,
+    ];
+
+    pub const SAMMLUNG_I_VIII: [Walze; 8] = [
+        Walze::I,
+        Walze::II,
+        Walze::III,
+        Walze::IV,
+        Walze::V,
+        Walze::VI,
+        Walze::VII,
+        Walze::VIII,
+    ];
+
     pub const I: Walze = Walze::new("EKMFLGDQVZNTOWYHXUSPAIBRCJ")
-        .mit_übertragungskerbe('Q'); // Turn the next rotor when the current rotor moves from Q to R
+        .mit_übertragungskerbe('Q') // Turn the next rotor when the current rotor moves from Q to R
+        .with_name("I");
     pub const II: Walze = Walze::new("AJDKSIRUXBLHWTMCQGZNPYFVOE")
-        .mit_übertragungskerbe('E');
+        .mit_übertragungskerbe('E')
+        .with_name("II");
     pub const III: Walze = Walze::new("BDFHJLCPRTXVZNYEIWGAKMUSQO")
-        .mit_übertragungskerbe('V');
+        .mit_übertragungskerbe('V')
+        .with_name("III");
     pub const IV: Walze = Walze::new("ESOVPZJAYQUIRHXLNFTGKDCMWB")
-        .mit_übertragungskerbe('J');
+        .mit_übertragungskerbe('J')
+        .with_name("IV");
     pub const V: Walze = Walze::new("VZBRGITYUPSDNHLXAWMJQOFECK")
-        .mit_übertragungskerbe('Z');
+        .mit_übertragungskerbe('Z')
+        .with_name("V");
     pub const VI: Walze = Walze::new("JPGVOUMFYQBENHZRDKASXLICTW")
         .mit_übertragungskerbe('Z')
-        .mit_übertragungskerbe('M');
+        .mit_übertragungskerbe('M')
+        .with_name("VI");
     pub const VII: Walze = Walze::new("NZJHGRCXMYSWBOUFAIVLPEKQDT")
         .mit_übertragungskerbe('Z')
-        .mit_übertragungskerbe('M');
+        .mit_übertragungskerbe('M')
+        .with_name("VII");
     pub const VIII: Walze = Walze::new("FKQHTLXOCBJSPDZRAMEWNIUYGV")
         .mit_übertragungskerbe('Z')
-        .mit_übertragungskerbe('M');
+        .mit_übertragungskerbe('M')
+        .with_name("VIII");
 
+    /// Creates a new rotor (Walze) with the specified mapping.
+    /// The mapping should be a string of 26 uppercase letters, where each letter represents the output for the corresponding input letter (A-Z).
     pub const fn new(mapping: &str) -> Self {
         Walze {
             inner_walze: Eintrittswalze::new(mapping),
@@ -93,12 +148,17 @@ impl Walze {
         }
     }
 
+    pub const fn with_name(mut self, name: &'static str) -> Self {
+        self.inner_walze = self.inner_walze.with_name(name);
+        self
+    }
+
     pub(crate) fn map_char(&self, c: u8, stellung: u8) -> u8 {
-        (self.inner_walze.inner_walze.mapping[(c + stellung) as usize % 26] + c) % 26
+        (c + self.inner_walze.inner_walze.mapping[(c + stellung) as usize % 26]) % 26
     }
 
     pub(crate) fn inverse_map_char(&self, c: u8, stellung: u8) -> u8 {
-        (self.inner_walze.inverse_mapping[(c + stellung) as usize % 26] + c) % 26
+        (c + self.inner_walze.inverse_mapping[(c + stellung) as usize % 26]) % 26
     }
 
     /// Adds a turnover notch (Übertragungskerbe) to the rotor at the specified character position. 
@@ -112,5 +172,21 @@ impl Walze {
 
     pub(crate) fn is_übertragungskerbe(&self, index: usize) -> bool {
         (self.übertragungskerben & (1 << index)) != 0
+    }
+}
+
+impl Deref for Walze {
+    type Target = Eintrittswalze;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner_walze
+    }
+}
+
+impl Deref for Eintrittswalze {
+    type Target = Umkehrwalze;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner_walze
     }
 }
